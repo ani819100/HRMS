1b042a7e1c599ead5c0a67a6dba65a11111d08bb
ani819100 <66264708+ani819100@users.noreply.github.com>
2026-03-11 18:56:06 +0530

    Adding orgin

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
